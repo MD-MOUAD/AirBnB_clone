d44eed8c57dbbeb0a729ac84e0316db7bfbbb138
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-10-25 01:07:53 +0100

    base_model_v1

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-"""BaseModel is a class that encapsulates shared attributes and methods,
+""" BaseModel is a class that encapsulates shared attributes and methods,
 serving as a blueprint for other classes. It manages the initialization,
 serialization, and deserialization of future instances, ensuring consistent
 handling of these aspects.
 """
-from datetime import datetime
 from uuid import uuid4
-import models
+from datetime import datetime
 
 
 class BaseModel:
@@ -14,7 +13,7 @@ class BaseModel:
     class BaseModel that defines all common
     attributes/methods for other classes
     """
-    def __init__(self, *args, **kwargs):
+    def __init__(self):
         """Constructor for the BaseModel class.
         Args:
             *args: Variable number of positional arguments.
@@ -23,16 +22,6 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        if len(kwargs) > 0:
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    if key in ["created_at", "updated_at"]:
-                        format = "%Y-%m-%dT%H:%M:%S.%f"
-                        self.__dict__[key] = datetime.strptime(value, format)
-                    else:
-                        self.__dict__[key] = value
-        else:
-            models.storage.new(self)
 
     def __str__(self):
         """
@@ -51,7 +40,6 @@ class BaseModel:
             None
         """
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
         """
@@ -62,10 +50,8 @@ class BaseModel:
             "__class__" key with the class name. Datetime objects are
             converted to ISO formatted strings.
         """
-        mydict = self.__dict__.copy()
-        mydict["__class__"] = self.__class__.__name__
-        if not isinstance(mydict["created_at"], str):
-            mydict["created_at"] = mydict["created_at"].isoformat()
-        if not isinstance(mydict["updated_at"], str):
-            mydict["updated_at"] = mydict["updated_at"].isoformat()
-        return mydict
+        dictionary = dict(self.__dict__)
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = datetime.isoformat(self.created_at)
+        dictionary["updated_at"] = datetime.isoformat(self.updated_at)
+        return dictionary
