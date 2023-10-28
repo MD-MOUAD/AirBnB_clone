0cacfef0128bc4e88f62c5b90a096b6de652fcb2
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2023-10-28 22:48:29 +0100

    update from dictionary

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Console module for AirBnB """
 import cmd
+import json
+import re
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -42,7 +44,7 @@ class HBNBCommand(cmd.Cmd):
         pass
 
     def method_error(self, name, arg):
-        """Handler for method's error
+        """Handler for method's error (helper function)
         return 0 if no error found otherwise 1
         """
         args = arg.split()
@@ -76,7 +78,7 @@ class HBNBCommand(cmd.Cmd):
         return 0
 
     def do_create(self, arg):
-        """Creates a new instance:
+        """Creates a new instance and print it's id:
 Usage: create <class name>
         """
         if not self.method_error("create", arg):
@@ -132,21 +134,30 @@ Usage: update <class name> <id> <attribute name> "<attribute value>"
         """
         if not self.method_error("update", arg):
             args = arg.split()
-            instance_key = args[0] + '.' + args[1]
+            class_name = args[0]      # <class name>
+            instace_id = args[1]      # <id>
+            attribute_name = args[2]  # <attribute name>
+            attr_value = args[3]      # <attribute value>
+            instance_key = class_name + '.' + instace_id
             obj = storage.all()[instance_key]
-            if args[3][0] == '"' and args[3][-1] == '"':
-                args[3] = args[3][1:-1]
-            if args[3][0] == "'" and args[3][-1] == "'":
-                args[3] = args[3][1:-1]
-            if hasattr(obj, args[2]):
-                data_type = type(getattr(obj, args[2]))
+            # remove "" and '' around <attribute value>
+            if (
+                attr_value[0] == '"' and attr_value[-1] == '"' or
+                attr_value[0] == "'" and attr_value[-1] == "'"
+            ):
+                attr_value = attr_value[1:-1]
+            # if <attribute name> is already exist then find it's type
+            if hasattr(obj, attribute_name):
+                # first store the attribute type in a variabe
+                data_type = type(getattr(obj, attribute_name))
                 try:
-                    args[3] = data_type(args[3])
-                    setattr(obj, args[2], args[3])
+                    # cast <attribute value> to it's attribute type
+                    attr_value = data_type(attr_value)
+                    setattr(obj, attribute_name, attr_value)
                 except ValueError:
-                    print(f"can't update {args[2]}: invalid type")
+                    print(f"can't update {attribute_name}: invalid type")
             else:
-                setattr(obj, args[2], args[3])
+                setattr(obj, attribute_name, attr_value)
             storage.save()
 
     def do_count(self, arg):
@@ -170,22 +181,44 @@ Usage: count <class name>
             "count": self.do_count
         }
         args = line.strip().split(".", 1)  # split only one time
-        if (
-            len(args) != 2 or
-            len(args[1]) < 3 or
-            args[1][-1] != ")"
-        ):
+        # example: "Place.update(89, latitude, 5.2)".split(".", 1)
+        # => ["Place", "update(89, latitude, 5.2)"] (to keep '.' of floats)
+        if len(args) != 2:
             return super().default(line)
         class_name = args[0]
         tmp = args[1].split('(')
+        # tmp = ["update", "89, latitude, 5.2)"]
         if len(tmp) < 2:
             return super().default(line)
-        method_name = tmp[0]
-        method_args = tmp[1][:-1]
+        method_name = tmp[0]                # => "update"
+        method_args = tmp[1].split(')')[0]  # => "89, latitude, 5.2"
         if method_name not in method_dict.keys():
             return super().default(line)
+        # handle update arguments
         if method_name == "update":
-            method_args = method_args.replace(',', ' ')
+            if '{' in method_args:  # Update from dictionary
+                tmp = method_args.split(',', 1)
+                if len(tmp) != 2  or '{' not in tmp[1] or '}' not in tmp[1]:
+                    return super().default(line)
+                instance_id = tmp[0]
+                if "'" in instance_id:
+                    instance_id = instance_id.replace("'", " ")
+                if '"' in instance_id:
+                    instance_id = instance_id.replace('"', " ")
+                instance_id = instance_id.strip()
+                json_dict_list = re.findall(r'{.*?}', tmp[1])
+                try:
+                    parced_dict = json.loads(json_dict_list[0])
+                    for attr_name, attr_value in parced_dict.items():
+                        final_arg = class_name + " " + instance_id + " "
+                        final_arg += str(attr_name) + " " + str(attr_value)
+                        self.do_update(final_arg) 
+                except json.JSONDecodeError:
+                    print(f"can't update: invalid type")
+                return
+
+            else:  # remove commas (',') and do a normal update
+                method_args = method_args.replace(',', ' ')
         final_arg = class_name + " " + method_args
         return method_dict[method_name](final_arg)
 
